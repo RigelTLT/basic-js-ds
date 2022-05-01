25fc8062377c14a1ddaed3eed1ab5ff6a34a02c9
RigelTLT <kozyrin-di@yandex.ru>
2022-05-01 20:23:17 +0400

    feat: complete queue task

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-// const { ListNode } = require('../extensions/list-node.js');
+ const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,24 +13,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
 
+class Queue {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return this.head;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  enqueue(value) {
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (this.length === 0) {
+      this.head = new ListNode(value);
+    } else {
+      let current = this.head;
+    
+    while (current.next) {
+      current = current.next;
+    }
+    current.next =new ListNode(value);}
+    this.length++;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
+    if(!this.head){
+      return null;
+    }
+    const current = this.head.value;
+    if(this.head.next){
+      this.head = this.head.next;
+    }
+    else{
+      this.head = null;
+    }
+    this.length--;
+    return current;
   }
 }
 
 module.exports = {
-  Queue
+  Queue,
 };
